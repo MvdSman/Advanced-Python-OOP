eba3e8cb5f62555c10785023104e2f23893e420a
Mark Van Der Sman <mark.vandersman@alten.nl>
2022-06-22 09:24:26 +0200

    Initialized projects after building App-1

---
+++ App-1-Geometry-Game/turtle_test.py
@@ -0,0 +1,19 @@
+import turtle_test
+
+# Create a canvas instance
+myturtle = turtle.Turtle()
+
+# Set starting position of turtle
+myturtle.penup()
+myturtle.goto(50, 75)
+myturtle.pendown()
+
+# Move turtle to create a square
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(100)
+turtle.done()
