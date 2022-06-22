b32cb6b1baa315bb487b9b6a6023f012755cb19a
Mark Van Der Sman <mark.vandersman@alten.nl>
2022-06-22 11:44:40 +0200

    Implemented PdfReport.generate()
    Basic PDF report with automatic PDF opening (Windows)

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -1,3 +1,6 @@
+import webbrowser
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contains data about a bill, such as the total amount and the period of the bill.
@@ -19,6 +22,7 @@ class Flatmate:
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         return round(bill.amount * weight, 2)
 
+
 class PdfReport:
     """
     Generates a PDF file which contains data regarding the flatmates, their due amount and the period of the bill.
@@ -27,7 +31,42 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        # Instantiate PDF file
+        pdf = FPDF(unit="pt")
+        pdf.add_page()
+
+        # Add content
+        ## Add logo
+        pdf.image("files/house.png", w=50, h=50)
+
+        ## Insert title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", align="C", ln=1)
+
+        ## Insert Period label + value
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=150, h=40, txt="Period:", border=1)
+        pdf.cell(w=200, h=40, txt=bill.period, border=1, ln=1)
+
+        ## Set font for table
+        pdf.set_font(family="Times", size=12)
+
+        ## Insert flatmate1 name + due
+        pdf.cell(w=150, h=40, txt=f"{flatmate1.name}:", border=1)
+        pdf.cell(w=200, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+
+        ## Insert flatmate2 name + due
+        pdf.cell(w=150, h=40, txt=f"{flatmate2.name}:", border=1)
+        pdf.cell(w=200, h=40, txt=str(flatmate2.pays(bill, flatmate1)), border=1, ln=1)
+
+        ## Insert Total due
+        pdf.set_font(family="Times", size=12, style="B")
+        pdf.cell(w=150, h=40, txt="Total due:", border=1)
+        pdf.cell(w=200, h=40, txt=str(bill.amount), border=1, ln=1)
+
+        # Export PDF and open file
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
 
 
 def main():
@@ -44,6 +83,9 @@ def main():
         f"John pays:\t{john.pays(bill=the_bill, flatmate2=mary)}\n"
         f"Mary pays:\t{mary.pays(bill=the_bill, flatmate2=john)}"
     )
+
+    pdf_report = PdfReport(filename="bill.pdf")
+    pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
 
 
 main()
