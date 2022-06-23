8251cd43315910c00ddf706bda35478602dd5286
Mark Van Der Sman <mark.vandersman@alten.nl>
2022-06-23 13:57:48 +0200

    Implemented BillForm and post method

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
-from flask import Flask, render_template
-from wtforms import Form
+from flask import Flask, render_template, request
+from wtforms import Form, StringField, SubmitField
 
 app = Flask(__name__)
 
@@ -10,15 +10,25 @@ class HomePage(MethodView):
 
 class BillFormPage(MethodView):
 	def get(self):
-		return render_template('bill_form_page.html')
+		bill_form = BillForm()
+		return render_template('bill_form_page.html', billform=bill_form)
 
 class ResultsPage(MethodView):
-	def get(self):
-		return render_template('results.html')
+	def post(self):
+		bill_form = BillForm(request.form)
+		return render_template('results.html', billform=bill_form)
 
 class BillForm(Form):
-	pass
+	amount = StringField('Bill Amount: ')
+	period = StringField('Bill Period: ')
 
+	name1 = StringField('Name: ')
+	days_in_house1 = StringField('Days in the house: ')
+
+	name2 = StringField('Name: ')
+	days_in_house2 = StringField('Days in the house: ')
+
+	button = SubmitField('Calculate')
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
