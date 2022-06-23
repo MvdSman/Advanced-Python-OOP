0ba06098b7b0150601b4004564b04be5f39bc210
Mark Van Der Sman <mark.vandersman@alten.nl>
2022-06-23 13:40:00 +0200

    Use template files for the web pages, implemented basic app

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -1,14 +1,27 @@
 from flask.views import MethodView
+from flask import Flask, render_template
 from wtforms import Form
 
+app = Flask(__name__)
+
 class HomePage(MethodView):
-	pass
+	def get(self):
+		return render_template('index.html')
 
 class BillFormPage(MethodView):
-	pass
+	def get(self):
+		return render_template('bill_form_page.html')
 
 class ResultsPage(MethodView):
-	pass
+	def get(self):
+		return render_template('results.html')
 
 class BillForm(Form):
 	pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+
+app.run(debug=True)
