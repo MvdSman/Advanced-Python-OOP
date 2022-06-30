901cb2691a36602215ad6fa7fb6f00cd0efdef3b
Mark van der Sman <mark.vandersman@alten.nl>
2022-06-30 16:02:49 +0200

    Implemented basic NewsFeed.get() for articles about interest
    Requires API key

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,7 +1,35 @@
+import requests
+from datetime import date, timedelta
+
 class NewsFeed:
+	"""
+	Object that gets news articles from the https://newsapi.org/v2/ API.
 
-	def __init__(self, data):
+	Requires an API key!
+	"""
+	def __init__(self, data, api_key='YOUR-API-KEY'):
 		self.data = data
+		self.api_key = api_key
 
-	def get(self):
-		pass
+	def _build_url(self, interest):
+		return f'https://newsapi.org/v2/everything?' \
+		       f'qInTitle={interest}&' \
+		       f'from={date.today() - timedelta(days=1)}&' \
+		       f'to={date.today()}&' \
+		       f'sortBy=popularity&' \
+		       f'apiKey={self.api_key}'
+
+	def get(self, interest):
+		"""
+		Gets the news articles of the last day based on an interest in the title.
+
+		:return: dictionary of articles.
+		"""
+		url = self._build_url(interest)
+		req = requests.get(url)
+		return req.json()['articles']
+
+
+if __name__ == "__main__":
+	news = NewsFeed(data='').get('The Netherlands')
+	print(news[0]['title'])
