a12a682df4bdf7133aad4601ea7f25451935e27a
Mark van der Sman <mark.vandersman@alten.nl>
2022-07-01 09:09:20 +0200

    Implemented Mailer class and NewsFeed.get()
    Expanded the main further

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,13 +1,19 @@
 import requests
 from datetime import date, timedelta
+import pandas as pd
+
+import emailer
 
 class NewsFeed:
 	"""
 	Object that gets news articles from the https://newsapi.org/v2/ API.
+	Default from the last day and english only.
 
 	Requires an API key!
 	"""
-	def __init__(self, from_date, to_date, language='en', api_key='YOUR-API-KEY'):
+	def __init__(self,
+	             from_date=date.today() - timedelta(days=1), to_date=date.today(),
+	             language='en', api_key='YOUR-API-KEY'):
 		self.from_date = from_date
 		self.to_date = to_date
 		self.language = language
@@ -22,28 +28,44 @@ class NewsFeed:
 		       f'sortBy=popularity&' \
 		       f'apiKey={self.api_key}'
 
-	def get(self, interest):
+	def get(self, interest, verbose=False):
 		"""
 		Gets the news articles of the last day based on an interest in the title.
 
 		:return: dictionary of articles.
 		"""
 		url = self._build_url(interest)
-		req = requests.get(url)
-		return req.json()['articles']
+		req = requests.get(url).json()['articles']
+
+		email_body = ''
+		print(interest)
+		if req:
+			for article in req:
+				email_body = f"{email_body}{article['title']}:\n\t" \
+				             f"{article['author']}\n\t" \
+				             f"{article['url']}\n\n"
+		else:
+			email_body = f"Sadly, no news updates were found for '{interest}'!\n\n"
+
+		if verbose:
+			print(email_body)
+		return email_body
 
 
 if __name__ == "__main__":
+
+	df = pd.read_excel('people.xlsx')
+	emailer = emailer.Mailer()
 	articles = NewsFeed(
-		from_date=date.today() - timedelta(days=1),
-		to_date=date.today(),
-		language='en'
-	).get('The Netherlands')
+		api_key='YOUR-API-KEY'
+	)
 
-	email_body = ''
-	for article in articles:
-		email_body = email_body + article['title'] + ':\n' +\
-		             '\t' + article['author'] + '\n' +\
-		             '\t' + article['url'] + '\n\n'
-
-	print(email_body)
+	for index, row in df.iterrows():
+		emailer.send(
+			to=row['email'],
+			subject=f"Your {row['interest']} news for today!",
+			contents=f"Hi {row['name']},\n\n"
+			         f"See what's on about {row['interest']} today.\n\n"
+			         f"{articles.get(row['interest'], verbose=False)}\n\n"
+			         f"Script Mailer."
+		)
